ba7a0f3820fe4657aec54b2f94ad6e30402749b2
Ouadie <elamraniouadie@gmail.com>
2023-12-21 12:41:27 +0100

    implementation of ATM and Main + creation interface operations

---
+++ src/git/atmproject/AtmOperationsInterf.java
@@ -0,0 +1,8 @@
+package git.atmproject;
+
+public interface AtmOperationsInterf {
+public void viewBalance();
+public void withdrawAmount(double withdrawAmount);
+public void depositAmount(double depositAmount);
+public void viewMiniStatement();
+}
--- src/git/atmproject/MainClass.java
+++ src/git/atmproject/MainClass.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
        int atmnumber = 12345;
        int atmpin = 123;
+       System.out.println("WELOCOME TO ATM");
        Scanner input = new Scanner(System.in);
        System.out.print("Enter ATM number : ");
        int atmNumber = input.nextInt();	
@@ -16,6 +17,27 @@ public class MainClass {
        
        if((atmnumber==atmNumber) && (atmpin==pin)) {
     	   System.out.print("Validation correct");
+    	   while(true) {
+    		   System.out.println("1 - Check Balance\n2 - Withdraw Amount\n3 - Deposit Amount\n4 - View ministatement\n 5 -  Exit");
+    		   System.out.print("Enter Choice : ");
+    		   int choice = input.nextInt();
+    		   switch(choice) {
+    		   case 1 : 
+    			   break;
+    		   case 2 : 
+    			   break;
+    		   case 3 : 
+    			   break;
+    		   case 4 : 
+    			   break;
+    		   case 5 :
+    			   System.out.print("Collect your Card\n Thank you for using our ATM");
+    			   System.exit(0);
+    			   break;
+    		   default :	   
+    			   System.out.print("Please enter a correct choice!");
+    		   }
+    	   }
        }else {
     	   System.out.print("Incorrect ATM number or pin");
        }
